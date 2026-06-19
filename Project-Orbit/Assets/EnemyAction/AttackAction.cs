93f3cb84167ac511a4aa41662fd9bbf74f9ba0ea
GAME\242j011 <abeharu0221@gmail.com>
2026-06-19 15:11:38 +0900

    Actionスクリプトの修正

--- Project-Orbit/Assets/EnemyAction/AttackAction.cs
+++ Project-Orbit/Assets/EnemyAction/AttackAction.cs
@@ -8,16 +8,18 @@ using UnityEngine;
 public class AttackAction : Enemy
 {
     [Header("“G‚ÌUŒ‚ƒAƒNƒVƒ‡ƒ“‚ª‹N‚«‚é”ÍˆÍ")]
-    [SerializeField] public float attackRange = 0f;
+    [SerializeField] public float attackRange = 0.0f;
 
     [Header("“G‚ÌUŒ‚ƒXƒRƒA")]
-    [SerializeField] public float score = 0f;
+    [SerializeField] public float score = 0.0f;
 
     [Header("UŒ‚‚Å‚«‚é‚©‚Ç‚¤‚©‚Ì”»’è")]
-    [SerializeField] public bool canAttack = false;
+    [SerializeField] public bool isCanAttack = false;
 
     public override float Evaluate(EnemyAIController enemy)
     {
+
+
         if (enemy.DistanceToTarget() <= attackRange)
         {
             // UŒ‚”ÍˆÍ“à‚É‚¢‚éê‡AƒXƒRƒA‚ğŒvZ‚µ‚Ä•Ô‚·
@@ -34,16 +36,18 @@ public class AttackAction : Enemy
 
         if (distance > attackRange)
         {
-            canAttack = false;
+            isCanAttack = false;
             enemy.agent.isStopped = false;
+
+            Debug.Log("UŒ‚”ÍˆÍŠO");
             return;
         }
 
-        canAttack = true;
+        isCanAttack = true;
         enemy.agent.isStopped = true;
         Debug.Log("UŒ‚");
     }
 
-    
-    
+
+
 }
